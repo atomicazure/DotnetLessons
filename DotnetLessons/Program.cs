cfbd4b3173e3214aafc50a28f5c5594ba7cb7803
priyanka <upretyprinka321@gmail.com>
2024-07-12 19:52:45 +0545

    Add project files.

---
+++ DotnetLessons/Program.cs
@@ -0,0 +1,23 @@
+﻿Introduction.DotnetLessons.MyCode.Program ob;
+
+namespace Introduction.DotnetLessons
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+namespace Introduction.DotnetLessons.MyCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
